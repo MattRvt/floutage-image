89be85876267d498b38fc85b83cb0a85b439c096
RIVAT Matthis <52105158+MattRvt@users.noreply.github.com>
2019-12-05 18:20:34 +0100

    floutage d'image

---
+++ src/definitionIntensite/Intensite.py
@@ -0,0 +1,110 @@
+import time
+from concurrent.futures import thread
+from multiprocessing import Process
+
+
+class Partie(Process):
+    # """Thread chargé simplement d'afficher un mot dans la console."""
+
+    def __init__(self, minY, maxY, im, h, w, numeroDeProcessus, queue):
+        Process.__init__(self)
+        self.queue = queue
+        self.minY = minY
+        self.maxY = maxY
+        self.im = im
+        self.h = h
+        self.w = w
+        self.i = numeroDeProcessus
+        self.imIntensite = [[0 for j in range(w)] for i in range(h)]
+
+    def run(self):
+        for y in range(self.minY, self.maxY):
+            for x in range(self.w):
+
+                # pour chaque pixel
+                rayonVoisinageStatic = 5
+                sommeR = 0
+                sommeG = 0
+                sommeB = 0
+                n = 0
+
+
+                #pixel du millieu
+                sommeR += self.im[y][x][0]
+                sommeG += self.im[y][x][1]
+                sommeB += self.im[y][x][2]
+                n = n + 1
+                for rayonVoisinage in range(0, rayonVoisinageStatic + 1):
+                    # si on atteint le pas bord en haut de l'immage
+                    if (y - rayonVoisinage >= 0):
+                        # px du haut
+                        sommeR += self.im[y - rayonVoisinage][x][0]
+                        sommeG += self.im[y - rayonVoisinage][x][1]
+                        sommeB += self.im[y - rayonVoisinage][x][2]
+                        n = n + 1
+
+                        # px haut gauche
+                        if (x - rayonVoisinageStatic >= 0):
+                            for xTemp in range(x - rayonVoisinageStatic, x + 1):
+                                sommeR += self.im[y - rayonVoisinage][xTemp][0]
+                                sommeG += self.im[y - rayonVoisinage][xTemp][1]
+                                sommeB += self.im[y - rayonVoisinage][xTemp][2]
+                                n = n + 1
+
+                        # px haut droit
+                        if (x + rayonVoisinageStatic < self.w):
+                            for xTemp in range(x + 1, x + rayonVoisinageStatic + 1):
+                                sommeR += self.im[y - rayonVoisinage][xTemp][0]
+                                sommeG += self.im[y - rayonVoisinage][xTemp][1]
+                                sommeB += self.im[y - rayonVoisinage][xTemp][2]
+                                n = n + 1
+
+                    #   si on atteint pas la limite bas de l'image
+                    if (y + rayonVoisinage < self.h):
+                        # px du bas
+                        sommeR += self.im[y + rayonVoisinage][x][0]
+                        sommeG += self.im[y + rayonVoisinage][x][1]
+                        sommeB += self.im[y + rayonVoisinage][x][2]
+                        n = n + 1
+
+                        # px haut gauche
+                        if (x - rayonVoisinage >= 0):
+                            for xTemp in range(x - rayonVoisinage, x + 1):
+                                sommeR += self.im[y + rayonVoisinage][xTemp][0]
+                                sommeG += self.im[y + rayonVoisinage][xTemp][1]
+                                sommeB += self.im[y + rayonVoisinage][xTemp][2]
+                                n = n + 1
+
+                        # px bas droit
+                        if (x + rayonVoisinageStatic < self.w):
+                            for xTemp in range(x + 1, x + rayonVoisinageStatic + 1):
+                                sommeR += self.im[y + rayonVoisinage][xTemp][0]
+                                sommeG += self.im[y + rayonVoisinage][xTemp][1]
+                                sommeB += self.im[y + rayonVoisinage][xTemp][2]
+                                n = n + 1
+
+                    # pixel a gauche
+                    if (x - rayonVoisinage >= 0):
+                        # px du haut
+                        sommeR += self.im[y][x - rayonVoisinage][0]
+                        sommeG += self.im[y][x - rayonVoisinage][1]
+                        sommeB += self.im[y][x - rayonVoisinage][2]
+                        n = n + 1
+                        #   si on atteint pas la limite bas de l'image
+                    if (x + rayonVoisinage < self.w):
+                        # px du bas
+                        sommeR += self.im[y][x + rayonVoisinage][0]
+                        sommeG += self.im[y][x + rayonVoisinage][1]
+                        sommeB += self.im[y][x + rayonVoisinage][2]
+                        n = n + 1
+
+                r = sommeR / n
+                g = sommeG / n
+                b = sommeB / n
+                self.imIntensite[y][x] = [r,g,b]
+
+        print("thread: ", self.i, " termine")
+        rep = [0, 0]
+        rep[0] = self.imIntensite
+        rep[1] = self.i
+        self.queue.put(rep)
---
+++ src/main.py
@@ -0,0 +1,61 @@
+import multiprocessing
+import os
+from copy import deepcopy
+from multiprocessing import Queue
+
+import imageio
+
+from definitionIntensite.Intensite import Partie
+
+if __name__ == '__main__':
+    print("debut du programme")
+    im = imageio.imread('chargementImage/arbreMaison.png', format='png')
+    info = im.shape
+    h = info[0]
+    w = info[1]
+    imIntensiteMoy = [[0 for j in range(w)] for i in range(h)]
+    queue = Queue()
+    # creation des parties de limages
+    nombreProcess = multiprocessing.cpu_count()
+    parties = []
+    for i in range(nombreProcess):
+        process = Partie(int(i * (h / nombreProcess)), int((i + 1) * (h / nombreProcess)), im, h, w, i, queue)
+        parties.append(process)
+
+    # demarage des thread
+    for i in range(nombreProcess):
+        parties[i].start()
+
+    # lecture des donnes
+    imIntensite = [[0] * len(im[1])] * len(im)  # le tableau a la meme taille que l'image d'origine
+
+    imRecu = []
+    for i in range(nombreProcess):
+        queueGet = queue.get()
+        imRecu = queueGet[0]
+        numProcess = queueGet[1]
+        imIntensite[int(numProcess * (h / nombreProcess)):int((numProcess + 1) * (h / nombreProcess))] \
+            = (imRecu[int(numProcess * (h / nombreProcess)):int((numProcess + 1) * (h / nombreProcess))])
+
+    # verification que les process se sont bien termine
+    for i in range(nombreProcess):
+        parties[i].join()
+
+    # definit le code RGB de chaque pixel
+    print("ecriture de l'image ")
+    for y in range(h):
+        for x in range(w):
+            im[y][x][0] = imIntensite[y][x][0]
+            im[y][x][1] = imIntensite[y][x][1]
+            im[y][x][2] = imIntensite[y][x][2]
+            """
+            if imIntensite[y][x] > 100:
+                im[y][x][0] = 0
+                im[y][x][1] = 0
+                im[y][x][2] = 0
+            else:
+                im[y][x][0] = 255
+                im[y][x][1] = 255
+                im[y][x][2] = 255
+                        """
+    imageio.imsave('chargementImage/france.png', im, format='png')
